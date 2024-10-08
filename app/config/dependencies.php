6b2dc856c7aa57b87cc7f3de10f1a8aac99ecb9e
Quentin <quentin.durand7@etu.univ-lorraine.fr>
2024-10-08 15:09:03 +0200

    ajustement

--- app/config/dependencies.php
+++ app/config/dependencies.php
@@ -6,16 +6,7 @@ use toubeelib\core\services\praticien\ServicePraticienInterface;
 use Monolog\Logger;
 use toubeelib\core\services\rdv\ServiceRendezvousInterface;
 
-// Définir les dépendances
-return [
-    // Autres définitions...
 
-    // Définition du service Rendezvous
-    ServiceRendezvousInterface::class => function ($c) {
-        return new ServiceRendezvous(
-            $c->get(RendezvousRepositoryInterface::class),
-            $c->get(ServicePraticienInterface::class),
-            $c->get('logger')
-        );
-    },
+return [
+
 ];
--- app/config/settings.php
+++ app/config/settings.php
@@ -1,47 +1,19 @@
 <?php
 
-use toubeelib\core\repositoryInterfaces\PraticienRepositoryInterface;
-use toubeelib\core\repositoryInterfaces\RendezvousRepositoryInterface;
-use toubeelib\core\services\rdv\ServiceRendezvous;
-use toubeelib\core\services\rdv\ServiceRendezvousInterface;
-use toubeelib\infrastructure\repositories\ArrayPraticienRepository;
-use toubeelib\infrastructure\repositories\ArrayRdvRepository;
 use Psr\Container\ContainerInterface;
-use toubeelib\application\actions\AnnulerRendezVousAction;
-use toubeelib\application\actions\RendezVousAction;
-use toubeelib\core\services\praticien\ServicePraticien;
-use toubeelib\core\services\praticien\ServicePraticienInterface;
 
 return  [
 
     'displayErrorDetails' => true,
     'logs.dir' => __DIR__ . '/../var/logs',
-    'log.prog.name' => 'prog.log',
-    /*
-    'prog.logger' => function(ContainerInterface $c) {
-    
-        $logger = new \Monolog\Logger($c->get('log.prog.name'));
-    $logger->pushHandler(
-    new \Monolog\Handler\StreamHandler(
-        $c->get('log.prog.file'),
-        $c->get('log.prog.level')));
-        return $logger;
-    },
-    */
 
-    RendezvousRepositoryInterface::class => new ArrayRdvRepository(),
-    PraticienRepositoryInterface::class => new ArrayPraticienRepository(),
-    ServicePraticienInterface::class => function(ContainerInterface $c){
-        return new ServicePraticien($c->get(PraticienRepositoryInterface::class));
+    'log.rdv.name' => 'rdv.log',
+    'logger.rdv.file' => function(ContainerInterface $c) {
+        return $c->get('logs.dir') . '/rdv.log';
     },
-    ServiceRendezvousInterface::class => function(ContainerInterface $c){
-        return new ServiceRendezvous($c->get(RendezvousRepositoryInterface::class), $c->get(ServicePraticienInterface::class));
-    },
-    RendezVousAction::class => function(ContainerInterface $c){
-        return new RendezVousAction($c->get(ServiceRendezvousInterface::class));
-    },
-    AnnulerRendezVousAction::class => function(ContainerInterface $c){
-        return new AnnulerRendezVousAction($c->get(ServiceRendezvousInterface::class));
-    }
+
+    'logger.rdv.level' => \Monolog\Level::Info,
+
+    'praticien.db.config' => 'praticien.db.ini'
 
     ] ;
