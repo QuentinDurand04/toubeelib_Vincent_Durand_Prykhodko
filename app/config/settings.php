d85d7f9f20228e5a1d05836076d88fcb96667873
Vincent GEORGES <vincent.georges3@etu.univ-lorraine.fr>
2024-10-08 15:42:04 +0200

    correction settings.php

--- app/config/settings.php
+++ app/config/settings.php
@@ -1,6 +1,17 @@
 <?php
 
+use toubeelib\core\repositoryInterfaces\PraticienRepositoryInterface;
+use toubeelib\core\repositoryInterfaces\RendezvousRepositoryInterface;
+use toubeelib\core\services\rdv\ServiceRendezvous;
+use toubeelib\core\services\rdv\ServiceRendezvousInterface;
+use toubeelib\infrastructure\repositories\ArrayPraticienRepository;
+use toubeelib\infrastructure\repositories\ArrayRdvRepository;
 use Psr\Container\ContainerInterface;
+use toubeelib\application\actions\AnnulerRendezVousAction;
+use toubeelib\application\actions\RendezVousAction;
+use toubeelib\core\services\praticien\ServicePraticien;
+use toubeelib\core\services\praticien\ServicePraticienInterface;
+
 
 return  [
 
@@ -14,6 +25,21 @@ return  [
 
     'logger.rdv.level' => \Monolog\Level::Info,
 
-    'praticien.db.config' => 'praticien.db.ini'
+    'praticien.db.config' => 'praticien.db.ini',
+
+    RendezvousRepositoryInterface::class => new ArrayRdvRepository(),
+    PraticienRepositoryInterface::class => new ArrayPraticienRepository(),
+    ServicePraticienInterface::class => function(ContainerInterface $c){
+        return new ServicePraticien($c->get(PraticienRepositoryInterface::class));
+    },
+    ServiceRendezvousInterface::class => function(ContainerInterface $c){
+        return new ServiceRendezvous($c->get(RendezvousRepositoryInterface::class), $c->get(ServicePraticienInterface::class));
+    },
+    RendezVousAction::class => function(ContainerInterface $c){
+        return new RendezVousAction($c->get(ServiceRendezvousInterface::class));
+    },
+    AnnulerRendezVousAction::class => function(ContainerInterface $c){
+        return new AnnulerRendezVousAction($c->get(ServiceRendezvousInterface::class));
+    }
 
     ] ;
