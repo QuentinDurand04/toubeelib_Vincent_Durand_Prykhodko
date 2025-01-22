a056878cadd92d295e8f0091b3fde65981f738c1
Quentin <quentin.durand7@etu.univ-lorraine.fr>
2025-01-22 16:05:53 +0100

    ajout micro-service auth

---
+++ app-auth/config/routes.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+use Slim\Exception\HttpNotFoundException;
+
+
+
+use toubeelib\application\actions\GetPatient;
+use toubeelib\application\actions\GetPraticien;
+use toubeelib\application\actions\GetRdvByPatient;
+
+use toubeelib\application\actions\PostSignIn;
+use toubeelib\middlewares\AuthnMiddleware;
+use toubeelib\middlewares\AuthzPatient;
+use toubeelib\middlewares\AuthzPraticiens;
+use toubeelib\middlewares\AuthzRDV;
+
+return function (\Slim\App $app): \Slim\App {
+
+    $app->get('/', \toubeelib\application\actions\HomeAction::class);
+
+    //RENDEZVOUS
+    $app->get('/rdvs[/]', \toubeelib\application\actions\GetAllRdvs::class)
+        ->setName('getAllRdvs');
+
+    $app->get('/praticiens/{id}/rdvs[/]', \toubeelib\application\actions\GetPraticienRdvs::class)
+        ->setName('rdvPraticien');
+
+    $app->post('/rdvs[/]', \toubeelib\application\actions\PostCreateRdv::class)
+        ->setName('createRdv');
+//        ->add(AuthnMiddleware::class);
+    ;
+
+    $app->get('/rdvs/{id}[/]', \toubeelib\application\actions\GetRdvId::class)
+        ->setName('getRdv');
+//        ->add(AuthzRDV::class)
+//        ->add(AuthnMiddleware::class);
+
+    //PATIENTS
+    $app->get('/patients/{id}/rdvs[/]', GetRdvByPatient::class)
+        ->setName('rdvPatient');
+//        ->add(AuthzPatient::class)
+//        ->add(AuthnMiddleware::class);
+
+    $app->get("/patients/{id}[/]", GetPatient::class)
+        ->setName('getPatient');
+//        ->add(AuthzPatient::class)
+//        ->add(AuthnMiddleware::class);
+
+    //PRATICIENS
+    $app->get('/praticiens[/]', \toubeelib\application\actions\GetAllPraticien::class)
+        ->setName('getAllPraticien');
+
+    /*$app->get('/praticiens/{id}/rdvs[/]', \toubeelib\application\actions\GetPraticienPlanning::class)
+        ->setName('planningPraticien');*/
+//        ->add(AuthzPraticiens::class)
+//        ->add(AuthnMiddleware::class);
+
+    $app->get("/praticiens/{id}[/]", GetPraticien::class )->setName('getPraticien');
+//        ->add(AuthnMiddleware::class);
+
+    $app->post('/signin[/]', PostSignIn::class)->setName('signIn');
+
+    $app->options('/{routes:.+}', function ($request, $response, $args) {
+        return $response;
+    });
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+        throw new HttpNotFoundException($request);
+    });    
+
+    return $app;
+};
---
+++ app-auth/src/core/repositoryInterfaces/PraticienRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace toubeelib\core\repositoryInterfaces;
+
+use DI\Container;
+use toubeelib\core\domain\entities\praticien\Praticien;
+use toubeelib\core\domain\entities\praticien\Specialite;
+
+interface PraticienRepositoryInterface
+{
+
+    public function __construct(Container $cont);
+    public function getSpecialiteById(string $id): Specialite;
+    public function getAllPraticiens(): array;
+    public function save(Praticien $praticien): string;
+    public function getPraticienById(string $id): Praticien;
+    public function searchPraticiens(Praticien $praticien): array;
+
+}
